f0b8e3e55f823fdc9d8c7ba18bdc3ed464914e34
Cadar <cadar.gabriel20@gmail.com>
2022-02-28 23:54:40 +0100

    ES Completato

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <NuovoComponente></NuovoComponente>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import NuovoComponente from './components/NuovoComponente.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    NuovoComponente
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/NuovoComponente.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Il mio primo componente !!!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NuovoComponente'
+}
+</script>
+
+<style>
+
+</style>
